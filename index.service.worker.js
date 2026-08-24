41b469253a52ae0f6907471ae32ba9116805403f
SillyGal <galsilly747@gmail.com>
2026-08-24 16:35:59 +0700

    AAAAAAAAAAAAAAAAAAAAA

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787562865|1132600467';
+const CACHE_VERSION = '1787563682|1949448683';
 /** @type {string} */
 const CACHE_PREFIX = 'oppositeday-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
@@ -17,7 +17,7 @@ const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
 const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 /** @type {string[]} */
-const CACHEABLE_FILES = ["index.wasm","index.pck","index.side.wasm"];
+const CACHEABLE_FILES = ["index.wasm","index.pck","index.side.wasm","libterrain.web.release.wasm32.wasm"];
 const FULL_CACHE = CACHED_FILES.concat(CACHEABLE_FILES);
 
 self.addEventListener('install', (event) => {
